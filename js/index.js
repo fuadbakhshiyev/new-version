183bc13f8c0155886b1bb459064eda0b8cbd938a
fuadbakhshiyev <fuad_7771@hotmail.com>
2021-02-15 23:00:38 +0400

    jsedit

--- js/index.js
+++ js/index.js
@@ -55,4 +55,16 @@ links.map(link => {
   });
 });
 
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
+gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".hero-content h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
+
+gsap.from(".shipping span", { opacity: 0, duration: 1, delay: 1.5, x: -20 ,y: -50 });
+gsap.from(".steps span", { opacity: 0, duration: 1, delay: 1.5, x: -100 });
+
+gsap.from(".ship h1", { opacity: 0, duration: 1, delay: 1, y: -45 ,x: -50 });
+gsap.from(".subtextemail", { opacity: 0, duration: 1, delay: 1, y: 45 ,x: 50 });
 
